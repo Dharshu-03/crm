863c96bf1eb24fda66afa182c6e30246fcc1812f
Dharshu-03 <dharshini.professional@gmail.com>
2026-04-11 20:19:00 +0530

    adding home frontend

--- src/Components/Emphome.jsx
+++ src/Components/Emphome.jsx
@@ -1,10 +1,84 @@
 import React from 'react'
 import Empnav from "./Empnav.jsx";
 import API from "../api.js";
+import './Emphome.css'
 
 const Emphome = () => {
     return (
         <div>
+            <div className="homeheading">
+                <div>
+                    <h3>Canvo</h3>
+                    <p>CRM</p>
+                </div>
+                <div>
+                    <h6>Good Morning</h6>
+                    <h1> name </h1>
+                </div>
+            </div>
+            <div className="homemain">
+                <h3>Timing</h3>
+
+                <div className="time">
+                    <div>
+                        <div className="checkin">
+                            <h5>Check in</h5>
+                            <h5>-----</h5>
+                        </div>
+                        <div className="checkin">
+                            <h5>Check out</h5>
+                            <h5>--:--</h5>
+                        </div>
+                    </div>
+                    <button className='inout'></button>
+
+                </div>
+
+
+                <div className="break">
+                    <div className="time">
+                        <div>
+                            <div className="checkin">
+                                <h5>Break</h5>
+                                <h5>--:--</h5>
+                            </div>
+                            <div className="checkin">
+                                <h5>Check out</h5>
+                                <h5>--:--</h5>
+                            </div>
+                        </div>
+                        <button className='breakbtn'></button>
+
+                    </div>
+                    <div className="pastbreaks">
+
+                        <div className="pastbreak">
+
+                            <div className="first">
+                                <div >
+                                    <h5>Break</h5>
+                                    <h5>--:--</h5>
+                                </div>
+                                <div >
+                                    <h5>Ended</h5>
+                                    <h5>--:--</h5>
+                                </div>
+                            </div>
+                            <div>
+                                <h5>Date</h5>
+                                <h5>--:--</h5>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <h3>Recent Activity</h3>
+
+                <div className="homeactivity"></div>
+
+            </div>
             <Empnav></Empnav>
         </div>
     )
